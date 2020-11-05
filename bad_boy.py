4987d27f684a4870a20e8b22195badfa49744b7e
joker192 <73433769+joker192@users.noreply.github.com>
2020-11-05 20:05:24 +0330

    Create bad_boy.py

---
+++ bad_boy.py
@@ -0,0 +1,88 @@
+import os
+import datetime
+now = datetime.datetime.now()
+green     = '\033[92m'
+red       = '\033[91m'
+while True:
+    print(f"{green}===============================")
+    print(f"{green}      made by bad_boy          ")
+    print(f"{green} my telegram id: @Bad_boy_468  ")
+    print(f"{green}===============================")
+    print(f"{green}[1]save a text ")
+    print(f"{green}[2]cracker")
+    print(f"{green}[3]fake hack")
+    print(f"{green}[4]game")
+    print("[5]install cracker for termux")
+    print("[6]install all pakage for termux")
+    print("[7]install metasploit for termux")
+    print("[8]install phishing script for termux")
+    print("[9]time")
+    print(f"{green}[10]exit")
+    joker = int(input(f"{red}[+]bad_boy==>"))
+    if joker == 1:
+        king=input(f"{green}Enter Your Text : ")
+        print(f"{red}Saved!")
+        jo=open("text.txt","a")
+        jo.write(king)
+        jo.write("|")
+        jo.close()
+    elif joker == 2:
+        you=int(input(f"{green}Enter Number 1 :"))
+        goo=int(input(f"{green}Enter Number 2 :"))
+        ppp=int(input(f"{green}Enter Number 3 :"))
+        iii=int(input(f"{green}Enter Number 4 :"))
+        yyy=int(input(f"{green}Enter Number 5 :"))
+        r=[]
+        r.append(you)
+        r.append(goo)
+        r.append(ppp)
+        r.append(iii)
+        r.append(yyy)
+        for a in range(0,5):
+            for k in range(0,5):
+                for y in range(0,5):
+                    for w in range(0,5):
+                        for u in range(0,5):
+                            print(r[a],r[k],r[y],r[w],r[u])
+    elif joker == 3:
+        kop=int(input(f"{red}Enter Your Target Number : "))
+        print(f"{red}start the hack....")
+        print(f"{red}LOADING■■■■■■■■")
+        print(f"{red}telegram code : 681738")  
+    elif joker == 4:
+        bb = input(f"{green}Enter Your name : ")
+        print(f"{red}Saved!")
+        kk = int(input(f"{green}Enter Your Number : "))
+        print(f"{red}Saved!")
+        print("■■■■■■■■■■■■■■■■■■■■■■")
+        print(f"{green}HELLO",bb)
+        print(f"{green}I Love You ",bb)
+        print(f"{green}I,m call you",kk,":)")
+        print(f"{red}■■■■■■■■■■■■■■■■■■■■■■")
+    elif joker == 10:
+        print(f"{red}■■■■■■■■■■■■■■■■■■■■■■■■■■")
+        print(f"{red}Enter (CTRL + c) to exit")
+        print(f"{red}■■■■■■■■■■■■■■■■■■■■■■■■■■")
+    elif joker == 5:
+        os.system("git clone https://github.com/Gameye98/Black-Hydra.git")
+        print(f"{green}Ended!")
+    elif joker == 6:
+        print(f"{red}started!")
+        os.system("apt update -y && apt upgrade -y ")
+        os.system("pkg install git -y")
+        os.system("pkg install python -y")
+        os.system("pkg install python2 -y")
+        os.system("pkg install nano -y ")
+        os.system("pkg install php -y")
+        os.system("pkg install bash -y ")
+        print(f"{green}ended!")
+    elif joker == 7:
+        print(f"{red}STARTED!")
+        os.system("pkg install metasploit -y")
+        print(f"{red}ENDED!")
+    elif joker == 8:
+        print(f"{green}started!")
+        os.system("git clone https://github.com/htr-tech/zphisher")
+        print(f"{green}Ended!")
+    elif joker == 9:
+        print(now)
